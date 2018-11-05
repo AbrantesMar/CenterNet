36e7f1d47c529a6b241815c889fc970878952948
Márcio Abrantes <abs.marcio@gmail.com>
2018-11-04 23:37:43 -0300

    Melhoria. para tratamento de exceção.

--- CenterApplicationTest/Program.cs
+++ CenterApplicationTest/Program.cs
@@ -14,72 +14,127 @@ namespace CenterApplicationTest
 
         static void Main(string[] args)
         {
-            
-            List<Employee> employeers = employeeService.GetEmployees();
-
-            Company company = new Company
+            try
             {
-                Id = 0,
-                Description = "Cliente 1",
-                FiscalYear = 2019
-            };
-            company.Clientes = clientService.GetClientByFile();
-            Tela(company, employeers);
+                List<Employee> employeers = employeeService.GetEmployees();
+
+                Company company = new Company
+                {
+                    Id = 0,
+                    Description = "Cliente 1",
+                    FiscalYear = 2019
+                };
+                company.Clientes = clientService.GetClientByFile();
+                Tela(company, employeers);
+
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                
+                Console.WriteLine(".------------------------------------------------------------------------------.");
+                Console.WriteLine("|  Erro ao acessar dados, favor entrar em contato com o suporte.               |");
+                Console.WriteLine("|  \\o Cadastra uma issuer relatando o que ouve com você:                       |");
+                Console.WriteLine(@"|  link: https://github.com/AbrantesMar/CenterNet/issues                       |");
+                Console.WriteLine("|  Antes de mandar, poderia ir lá na pasta \"C:\\carga\"?                         |");
+                Console.WriteLine("|  Chegando lá, da uma olhada se existem dois arquivos chamados:               |");
+                Console.WriteLine("|        employees.txt e team.txt                                              |");
+                Console.WriteLine("|  Se concluiu os passos, clica 1 que volta para o menu. ;) obrigado.          |");
+                Console.WriteLine("|  Caso exista, conseguiu chegar seu template?                                 |");
+                Console.WriteLine("|  Na camada de Repository, existe os dois templates para lhe ajudar.          |");
+                Console.WriteLine("'------------------------------------------------------------------------------'");
+                int acessarmenu = 0;
+                acessarmenu = Convert.ToInt32(Console.ReadLine());
+                if (acessarmenu == 1)
+                {
+                    Console.Clear();
+                    Main(new string[0]);
+                }
+                
+            }
             
-            Console.ReadLine();
         }
+
         public static void Tela(Company company, List<Employee> employees)
         {
-            Console.WriteLine(".------------------------------------------------------------------------------.");
-            Console.WriteLine("|---  ClientTime                                                               |");
-            Console.WriteLine("|Opções:                                                                       |");
-            Console.WriteLine("|-   2- Promover                                                               |");
-            Console.WriteLine("|-   3- Recalibrar                                                             |");
-            Console.WriteLine("|-   4- Montar time                                                            |");
-            Console.WriteLine("|-   5- Visualizar Resultados                                                  |");
-            Console.WriteLine("|-   6- limpar Tela                                                            |");
-            Console.WriteLine("|-   Digite o que deseja.                                                      |");
-            Console.WriteLine("'------------------------------------------------------------------------------'");
-            switch (Console.ReadLine())
-            {
-                case "2":
-                    Console.WriteLine(".---------------- Digite a quantidade a serem promovidos ----------------------.");
-                    int quantityPromotions = 0;
-                    quantityPromotions = Convert.ToInt32(Console.ReadLine());
-                    employees = employeeService.Progression(employees, quantityPromotions);
-                    clientService.FormulateTime(company.Clientes, employees);
-                    clientService.MergeRevert(company.Clientes);
-                    Console.Clear();
-                    Console.WriteLine(".------------------------- Processado com sucesso -----------------------------.");
-                    Console.WriteLine(".------------------------------------------------------------------------------.");
-                    break;
-                case "3":
-                    clientService.MergeRevert(company.Clientes);
-                    Console.Clear();
-                    Console.WriteLine(".------------------------- Processado com sucesso -----------------------------.");
-                    Console.WriteLine(".------------------------------------------------------------------------------.");
-                    break;
-                case "4":
-                    clientService.FormulateTime(company.Clientes, employees);
-                    Console.Clear();
-                    Console.WriteLine(".------------------------- Processado com sucesso -----------------------------.");
-                    Console.WriteLine(".------------------------------------------------------------------------------.");   
-                    break;
-                case "5":
-                    Console.WriteLine(".------------------------- Click para sair da tela -----------------------------.");
-                    WriteResult(company);
-                    Console.ReadLine();
-                    Console.Clear();
-                    Console.WriteLine(".------------------------- Processado com sucesso -----------------------------.");
-                    Console.WriteLine(".------------------------------------------------------------------------------.");
-                    break;
-                case "6":
-                    Console.Clear();
-                    break;
-                default:
-                    break;
+            try
+            { 
+                Console.WriteLine(".------------------------------------------------------------------------------.");
+                Console.WriteLine("|---  ClientTime                                                               |");
+                Console.WriteLine("|Opções:                                                                       |");
+                Console.WriteLine("|-   2- Promover                                                               |");
+                Console.WriteLine("|-   3- Recalibrar                                                             |");
+                Console.WriteLine("|-   4- Montar time                                                            |");
+                Console.WriteLine("|-   5- Visualizar Resultados                                                  |");
+                Console.WriteLine("|-   6- limpar Tela                                                            |");
+                Console.WriteLine("|-   Digite o que deseja.                                                      |");
+                Console.WriteLine("'------------------------------------------------------------------------------'");
+                switch (Console.ReadLine())
+                {
+                    case "2":
+                        Console.WriteLine(".---------------- Digite a quantidade a serem promovidos ----------------------.");
+                        int quantityPromotions = 0;
+                        quantityPromotions = Convert.ToInt32(Console.ReadLine());
+                        employees = employeeService.Progression(employees, quantityPromotions);
+                        company.FiscalYear = company.FiscalYear + 1;
+                        clientService.FormulateTime(company.Clientes, employees);
+                        clientService.MergeRevert(company.Clientes);
+                        Console.Clear();
+                        Console.WriteLine(".------------------------- Processado com sucesso -----------------------------.");
+                        Console.WriteLine(".------------------------------------------------------------------------------.");
+                        break;
+                    case "3":
+                        clientService.MergeRevert(company.Clientes);
+                        Console.Clear();
+                        Console.WriteLine(".------------------------- Processado com sucesso -----------------------------.");
+                        Console.WriteLine(".------------------------------------------------------------------------------.");
+                        break;
+                    case "4":
+                        clientService.FormulateTime(company.Clientes, employees);
+                        Console.Clear();
+                        Console.WriteLine(".------------------------- Processado com sucesso -----------------------------.");
+                        Console.WriteLine(".------------------------------------------------------------------------------.");   
+                        break;
+                    case "5":
+                        Console.WriteLine(".------------------------- Click para sair da tela -----------------------------.");
+                        WriteResult(company);
+                        Console.ReadLine();
+                        Console.Clear();
+                        Console.WriteLine(".------------------------- Processado com sucesso -----------------------------.");
+                        Console.WriteLine(".------------------------------------------------------------------------------.");
+                        break;
+                    case "6":
+                        Console.Clear();
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine(".--------- Opa, esta opção não existe, tenta uma dessas da tela ---------------.");
+                        Console.WriteLine(".------------------------------------------------------------------------------.");
+                        break;
+                }
             }
-            Tela(company, employees);
+            catch (Exception)
+            {
+                Console.WriteLine("Erro ao entrar no meu, favor entrar em contato com o suporte.");
+                Console.WriteLine(".------------------------------------------------------------------------------.");
+                Console.WriteLine("|  Erro ao acessar dados, favor entrar em contato com o suporte.               |");
+                Console.WriteLine("|  \\o Cadastra uma issuer relatando o que ouve com você:                       |");
+                Console.WriteLine(@"|  link: https://github.com/AbrantesMar/CenterNet/issues                       |");
+                Console.WriteLine("|  Clica 1 que volta para o menu. Vamos tentar novamente ;) obrigado.           |");
+                Console.WriteLine("'------------------------------------------------------------------------------'");
+                int acessarmenu = 0;
+                acessarmenu = Convert.ToInt32(Console.ReadLine());
+                if (acessarmenu == 1)
+                {
+                    Console.Clear();
+                    Main(new string[0]);
+                }
+                Console.ReadLine();
+            }
+            finally
+            {
+                Tela(company, employees);
+            }
         }
 
         public static void WriteResult(Company company)
--- CenterRepository/EmployeeRepository.cs
+++ CenterRepository/EmployeeRepository.cs
@@ -18,34 +18,43 @@ namespace CenterRepository
 
         public List<Employee> GetEmployeesByFile()
         {
-            string[] lines = System.IO.File.ReadAllLines(@"C:\carga\employees.txt");
-
-            var employees = new List<Employee>();
-            string[] lineArray;
-
-
-            for (int i = 0, count = lines.Length; i < count; i++)
+            try
             {
-                //Nome do Empregado, Nível, Ano de Nascimento, Ano de Admissão, Último Ano de Progressão 
-                lineArray = lines[i].Split(',');
-                var employee = new Employee();
-                employee.Id = i;
-                employee.Description = lineArray[0];
-                int aux;
-                int.TryParse(lineArray[3].Trim().ToString(), out aux);
-                employee.AdmissionYear = aux;
-                int.TryParse(lineArray[2].Trim().ToString(), out aux);
-                employee.BirthYear = aux;
-                int.TryParse(lineArray[4].Trim().ToString(), out aux);
-                employee.LastProgressionYear = aux;
-                int.TryParse(lineArray[1].Trim().ToString(), out aux);
-                employee.PLevel = aux;
+                string[] lines = System.IO.File.ReadAllLines(@"C:\carga\employees.txt");
 
-                employees.Add(employee);
+                var employees = new List<Employee>();
+                string[] lineArray;
 
+
+                for (int i = 0, count = lines.Length; i < count; i++)
+                {
+                    //Nome do Empregado, Nível, Ano de Nascimento, Ano de Admissão, Último Ano de Progressão 
+                    lineArray = lines[i].Split(',');
+                    var employee = new Employee();
+                    employee.Id = i;
+                    employee.Description = lineArray[0];
+                    int aux;
+                    int.TryParse(lineArray[3].Trim().ToString(), out aux);
+                    employee.AdmissionYear = aux;
+                    int.TryParse(lineArray[2].Trim().ToString(), out aux);
+                    employee.BirthYear = aux;
+                    int.TryParse(lineArray[4].Trim().ToString(), out aux);
+                    employee.LastProgressionYear = aux;
+                    int.TryParse(lineArray[1].Trim().ToString(), out aux);
+                    employee.PLevel = aux;
+
+                    employees.Add(employee);
+
+                }
+
+                return employees;
             }
+            catch (System.Exception)
+            {
 
-            return employees;
+                throw;
+            }
+            
         }
     }
 }
